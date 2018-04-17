c74fb1d90d43b31a80adf29dfa783879207649a9
CarlosHdz97 <CarlosHdz_97@outlook.com>
2018-04-17 16:27:48 -0500

    mejoras

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const express = require("express");
 const app = express();
 const productRoutes = require('./routes/product.js');
 const provedorRoutes = require('./routes/provedor.js');
+const productProvedor = require('./models/productProvedor.js');
 const productOrder = require('./models/productOrder.js');
 
-//const pp = require('./models/productProvedor.js');
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,7 +17,7 @@ app.use('/provedor',provedorRoutes);
 app.use('/product',productRoutes);
 
 
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 console.log(port);
 app.listen(port, () => {
   console.log('Servidor iniciado en el puerto: ', port);
