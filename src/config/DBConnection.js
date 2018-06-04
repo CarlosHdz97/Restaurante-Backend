97d9adef488ae04142045666a5383c9c2cfe08db
CarlosHdz97 <CarlosHdz_97@outlook.com>
2018-06-04 14:54:21 -0500

    base de datos heroku

--- src/config/DBConnection.js
+++ src/config/DBConnection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 //const connection = new Sequelize('mysql://root:CARLOS@localhost:3306/restaurante');
-
+const connection = new Sequelize('mysql://b3525d3e84c298:81d10a2c@us-cdbr-iron-east-05.cleardb.net/heroku_1d483ea445fb124?reconnect=true');
 //const connection = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 connection.authenticate().then(() => {
     console.log('Conexión establecida exitosamente.');
